77668de5539771a1472b7b830fa2aa22239a0a15
gervit1989 <gervit2008@mail.ru>
2025-03-12 13:31:34 +0300

    2015-12-01 - часть 2

--- 2015/python/December-01/target.py
+++ 2015/python/December-01/target.py
@@ -19,4 +19,30 @@ def part1_of_task():
         print('res for row ', row[:25], " is : ",floor_id)
 
 # часть 1
+def part2_of_task():
+    in_arr = read_data('input.txt')
+    for row in in_arr:
+        lst = list(row)
+        floor_id=0
+        pos = 0
+        fpos = 0
+        flag_found = False
+        print('len:', len(lst))
+        for letter in lst:
+            if letter == '(':
+                floor_id+=1
+            elif letter == ')':
+                floor_id-=1
+            if floor_id == -1 and not flag_found:
+                fpos = pos+1
+                print('pos: ', pos)
+                flag_found = True
+            pos+=1
+
+        print('res for row ', row[:25], " is : ",fpos)
+
+
+# часть 1
 part1_of_task()
+# часть 2
+part2_of_task()
