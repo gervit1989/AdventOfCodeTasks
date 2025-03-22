15d232c158449afa6a9ab020ba017c8198d10228
gervit1989 <gervit2008@mail.ru>
2025-03-22 11:39:39 +0300

    2015-12-05

--- 2015/python/December-04/target.py
+++ 2015/python/December-04/target.py
@@ -7,10 +7,9 @@ def read_data(file_name):
         in_data = [row.strip() for row in fin.readlines()]
     return in_data
 
-
 # часть 1
-def part1_of_task(name_arg:str, fstr:str, col_count:int):
-    in_arr = read_data(name_arg)
+def part1_of_task(arg_name:str, arg_str:str):
+    in_arr = read_data(arg_name)
     sum_ans = 0
     for row in in_arr:
         lst1 = row.split('=')
@@ -18,15 +17,15 @@ def part1_of_task(name_arg:str, fstr:str, col_count:int):
         if len(lst1) > 1:
             answer = int(lst1[1])
         #ival = 10**(len(lst1[0])-1)
-        ival = 0
+        num_val = 0
         while True:
-            str_data1 = str(lst1[0]) + str(ival)
+            str_data1 = str(lst1[0]) + str(num_val)
             res = hashlib.md5(str_data1.encode()).hexdigest()
             #print("res: ", ival, str_data1, res)
-            if res.startswith(fstr):
+            if res.startswith(arg_str):
                 break
-            ival+=1
-        answ = ival
+            num_val+=1
+        answ = num_val
         print('on row ', answ, answ - answer)
         sum_ans += answ - answer
     print('ans of part1:= ', sum_ans)
@@ -34,6 +33,6 @@ def part1_of_task(name_arg:str, fstr:str, col_count:int):
 
 name_arg = 'in.txt'
 str_find = '00000'
-part1_of_task(name_arg, str_find, 5)
+part1_of_task(name_arg, str_find)
 str_find = '000000'
-part1_of_task(name_arg, str_find, 6)
+part1_of_task(name_arg, str_find)
---
+++ 2015/python/December-05/target.py
@@ -0,0 +1,92 @@
+# чтение данных
+def read_data(file_name):
+    in_data = None
+    with open(file_name, 'r') as fin:
+        in_data = [row.strip() for row in fin.readlines()]
+    return in_data
+
+
+# Количество букв из списка
+def get_count_of_letters_in_lst(line_str: str, lst: list = None):
+    icount = 0
+    for letter in lst:
+        icount += line_str.count(letter)
+    return icount
+
+def rule1(line_str:str):
+    letter_count = get_count_of_letters_in_lst(line_str, list('aeiou'))
+    case2 = False
+    case3 = True
+    if letter_count >= 3:
+        alphabets = []
+        for i in range(97, 123):
+            alphabets.append(chr(i))
+        alphabet_size = len(alphabets)
+        for i in range(alphabet_size):
+            str_init = alphabets[i]
+            str1 = str_init + str_init
+            if line_str.count(str1):
+                case2 = True
+        lst_not_allowed = ['ab', 'cd', 'pq', 'xy']
+        for comb in lst_not_allowed:
+            if line_str.count(comb):
+                case3 = False
+        if case2 and case3:
+            return 1
+    return 0
+
+def rule2(line_str:str):
+    alphabets = []
+    for i in range(97, 123):
+        alphabets.append(chr(i))
+    alphabet_size = len(alphabets)
+    case1 = False
+    case2 = False
+    for letter in alphabets:
+        if line_str.count(letter)>=2:
+            pos2 = 0
+            while True:
+                pos1 = line_str.find(letter, pos2)
+                pos2 = line_str.find(letter, pos1+1)
+                #print(pos1, pos2)
+                if pos2 == pos1+2:
+                    case2 = True
+                    break
+                if pos2 == -1:
+                    break
+        for letter2 in alphabets:
+            comb = letter+letter2
+            if line_str.count(comb)>=2:
+                pos1l = line_str.find(letter)
+                pos2l = line_str.find(letter, pos1l+2)
+                if pos2l >=0:
+                    case1 = True
+    #print(case1, case2)
+    if case1 and case2:
+        return 1
+    return 0
+
+# часть 1
+def part1_of_task(arg_name: str, num_rule:int):
+    in_arr = read_data(arg_name)
+    sum_ans = 0
+    for row in in_arr:
+        lst1 = row.split('=')
+        answer = 0
+        if len(lst1) > 1:
+            answer = int(lst1[1])
+        answer_var = 0
+        line_var = lst1[0]
+        if num_rule == 1:
+            answer_var = rule1(line_var)
+        elif num_rule == 2:
+            answer_var = rule2(line_var)
+        #print(letter_count, case2, case3)
+        print('on row ', answer_var, answer_var - answer)
+        sum_ans += answer_var - answer
+    print('ans of part1:= ', sum_ans)
+
+
+file_name = 'input.txt'
+#part1_of_task(file_name, 1)
+part1_of_task(file_name, 2)
